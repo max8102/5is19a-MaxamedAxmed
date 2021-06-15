a8cbb365d3a552d1c5fa979c04ea7ded38eeae4d
max8102 <77672399+max8102@users.noreply.github.com>
2021-06-15 20:38:11 +0200

    Echo Server - Version 1.1

--- EchoServer/EchoServer/src/MyEchoServer.java
+++ EchoServer/EchoServer/src/MyEchoServer.java
@@ -22,7 +22,7 @@ public class MyEchoServer extends Thread {
 		
 		ServerSocket serverSocket = null;
 
-		int serverPort = 108;
+		int serverPort = 1080;
         int myTimeout = 10000;
         int quietFlag = 0;
         
